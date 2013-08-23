15775af7e1004e22590a1d9acf926b5356d9f3ec
Kaspars Dambis <kaspars@konstruktors.com>
2013-08-23 09:42:14 +0000

    Add backend options

--- script-helper.php
+++ script-helper.php
@@ -9,25 +9,79 @@ Author URI: http://konstruktors.com
 */
 
 
-register_activation_hook( __FILE__, 'script_helper_activate' );
-
-function script_helper_activate() {
-
-	add_option( 'script_helper_header', '' );
-	add_option( 'script_helper_footer', '' );
-
-}
-
-
 add_action( 'wp_footer', 'script_helper_footer', 20 );
 
 function script_helper_footer() {
 
-	$scripts = trim( get_option( 'script_helper_footer', '' ) );
+	$script = trim( get_option( 'script_helper_footer' ) );
 
-	if ( empty( $scripts ) )
+	if ( empty( $script ) )
 		return;
 
-	echo $script;
+	printf( "\n<!-- Script helper footer: -->\n%s\n\n", $script );
 
 }
+
+
+add_action( 'wp_header', 'script_helper_header', 20 );
+
+function script_helper_header() {
+
+	$script = trim( get_option( 'script_helper_header' ) );
+
+	if ( empty( $script ) )
+		return;
+
+	printf( "\n<!-- Script helper header: -->\n%s\n\n", $script );
+
+}
+
+
+add_action( 'admin_init', 'register_script_helper_settings' );
+
+function register_script_helper_settings() {
+
+	if ( ! current_user_can( 'install_plugins' ) )
+		return;
+
+	register_setting( 'general', 'script_helper_header' );
+	register_setting( 'general', 'script_helper_footer' );
+
+	add_settings_field(
+		'script_helper_settings', 
+		__( 'Scripts', 'script-helper' ), 
+		'script_helper_settings', 
+		'general', 
+		'default' 
+	);
+
+}
+
+function script_helper_settings() {
+
+	$scripts = array(
+		'script_helper_header' => array(
+			'title' => __( 'Header', 'script-helper' ),
+			'value' => get_option( 'script_helper_header' )
+		),
+		'script_helper_footer' => array(
+			'title' => __( 'Footer', 'script-helper' ),
+			'value' => get_option( 'script_helper_footer' )
+		)
+	);
+
+	foreach  ( $scripts as $option_key => $option )
+		printf(
+			'<p>
+			<label>
+				<strong>%s</strong>
+				<textarea name="%s" rows="10" class="large-text code">%s</textarea>
+			</label>
+			</p>',
+			esc_html( $option['title'] ),
+			esc_attr( $option_key ),
+			esc_html( $option['value'] )
+		);
+
+}
+
